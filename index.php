a9b064018562619117544ca4901a0d0ac910e0c9
Nayan <nayanmailbox@gmail.com>
2020-04-06 20:08:32 +0530

    Code Changes

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <p> Hello World, PHP in Azure. For getting phpinfo click <a href="phpinfo.php">phpinfo<a> </p>
 <p> We are testing with Blue Green Deployments in Azure </p>
+<p> Azure App Service for Linux </p>
